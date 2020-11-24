8ec35e4bbad9d475b304fcfe6e81ac267b683d72
phn210 <57532941+phn210@users.noreply.github.com>
2020-11-24 14:46:06 +0700

    Update img and css

--- src/controllers/DBConnection.java
+++ src/controllers/DBConnection.java
@@ -1,4 +1,23 @@
 package controllers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static Connection connection;
+    private static String connectionURL = "jdbc:sqlserver://THINKPAD;database=QuanLyCapPhanThuong;";
+    private static String user = "xanh";
+    private static String password = "jaunebleu";
+
+    public static void main(String[] args){
+        try {
+            connection = DriverManager.getConnection(connectionURL, user, password);
+            System.out.println("Database successfully connected!");
+        }
+        catch(SQLException e){
+            System.out.println("Some errors occurred!");
+            e.printStackTrace();
+        }
+    }
 }
