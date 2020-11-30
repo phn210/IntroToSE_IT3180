9ea6bd022051611bb7080473e8db74ce6b9a5d82
Tran Huy <tranhuuhuy297@gmail.com>
2020-12-01 03:24:24 +0700

    fix sthg

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,15 +1,10 @@
 package controllers;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableArray;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.layout.BorderPane;
@@ -18,7 +13,6 @@ import views.Main;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
 
 public class MainController{
     @FXML
