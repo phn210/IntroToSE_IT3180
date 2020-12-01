c6adfb585af14bd14fad47b5fb56910b194629fe
Tran Huy <tranhuuhuy297@gmail.com>
2020-12-01 18:46:25 +0700

    fix sthg

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # IntroToSE_IT3180
 Project for Introduction to Software Engineering course (IT3180)
+##
+* Set password SQL User 'sa' to None
+* Set TCP Port to 1433
--- src/controllers/HoGiaDinhController.java
+++ src/controllers/HoGiaDinhController.java
@@ -3,7 +3,6 @@ package controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
